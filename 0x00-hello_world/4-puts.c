d09b9d6b4665f4b569ff161f4a4af277998c37b9
Olooce <asirastephen9@gmail.com>
2023-10-05 01:50:51 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,21 @@
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints a message to the standard output
+ * using the `puts` function and then returns 0 to indicate success.
+ *
+ * Return: Always 0 (Success)
+ */
 #include <stdio.h>
 
 int main(void)
 {
+    /**
+     * The `puts` function is used to print the message to the
+     * standard output (the console). It automatically appends
+     * a newline character at the end.
+     */
     puts("Programming is like building a multilingual puzzle");
+
     return (0);
 }
