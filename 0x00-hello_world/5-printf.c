f27c91063d81c939c61574cc38ddd7ea98887773
Olooce <asirastephen9@gmail.com>
2023-10-05 01:53:26 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints a message to the standard output
+ * with proper grammar, followed by a new line, as a piece of art.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
