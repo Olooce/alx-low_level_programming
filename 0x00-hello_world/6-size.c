ef2242a0e8c28258226ffc3299c8e4f983cb7f2c
Olooce <asirastephen9@gmail.com>
2023-10-06 05:49:44 -0700

    5

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,12 +10,8 @@
  */
 int main(void)
 {
-    /**
-     * The `puts` function is used to print the message to the
-     * standard output (the console). It automatically appends
-     * a newline character at the end.
-     */
-    puts("Programming is like building a multilingual puzzle");
+	char *message = "Programming is like building a multilingual puzzle";
 
-    return (0);
+	puts(message);
+	return (0);
 }
